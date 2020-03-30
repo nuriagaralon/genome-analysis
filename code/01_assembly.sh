f39dec5733e29a8313aa2ada0b5d1b680c4c7fe1
nuria <60143046+nuriagaralon@users.noreply.github.com>
2020-03-30 15:01:52 +0200

    01_assembly.sh code

---
+++ code/01_assembly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 12:00:00
+#SBATCH -J 01_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user nuria.garrigaalonso.5928@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load canu
+
+# running canu
+canu \
+ -p lferriphilum \
+ -d genome-analysis/analyses/01_genome_assembly/01_assembly_canu \
+ genomeSize=2.6m \
+ stopOnReadQuality=false \
+ maxThreads=4 \
+ -pacbio-raw genome-analysis/data/DNA_raw_data/ERR2028*.fastq.gz 
+
