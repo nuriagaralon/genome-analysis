6e8feb40ccf900c4dccc1cfa9cc4c0827bbc3a71
nuria <60143046+nuriagaralon@users.noreply.github.com>
2020-04-03 13:32:56 +0200

    mummer code

--- code/0103_eval_mummer.sh
+++ code/0103_eval_mummer.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -A g2020008
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 00:01:00
+#SBATCH -n 1
+#SBATCH -t 00:10:00
 #SBATCH -J 03_eval_mummer
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user nuria.garrigaalonso.5928@student.uu.se
@@ -12,7 +12,17 @@
 module load bioinfo-tools
 module load MUMmer/3.23
 
+# running MUMmer
+mummer -mum -b -c \
+data/lferriphilum_ref.fasta \ 
+analyses/01_genome_assembly/01_assembly_canu/lferriphilum.contigs.fasta \
+> analyses/01_genome_assembly/03_eval_mummer/ref_qry.mums 
 
 # running mummerplot
-#probably need to run nucmer?
-mummerplot -Q analyses/01_genome_assembly/01_assembly_canu/lferriphilum.contigs.fasta -R data/lferriphilum_ref.fasta
+mummerplot \
+-Q analyses/01_genome_assembly/01_assembly_canu/lferriphilum.contigs.fasta \
+-R data/lferriphilum_ref.fasta \
+analyses/01_genome_assembly/03_eval_mummer/lferriphilum.mums \
+--postscript --prefix=lferriphilum
+
+gnuplot lferriphilum.gp
