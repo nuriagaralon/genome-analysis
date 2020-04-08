9bf69195a2d0c64404216ae94758626088d8c8b9
Núria Garriga Alonso <nuga5928@rackham3.uppmax.uu.se>
2020-04-08 13:44:37 +0200

    blastx for initial protein

---
+++ analyses/01_genome_assembly/08_evalcirc_blast/0108_evalcirc_blast.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J 0108_evalcirc_blast
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user nuria.garrigaalonso.5928@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load blast
+
+# variables contig 59
+FOLDER="~/genome-analysis/analyses/01_genome_assembly/08_evalcirc_blast"
+
+# Run blast
+blastx -db refseq_protein_v5 \
+-query lferr_beginning_ref.fasta \
+-out lferr_prot_ref.txt \
+-num_threads 4
+
+blastx -db refseq_protein_v5 \
+-query lferr_beginning_circ.fasta \
+-out lferr_prot_circ.txt \
+-num_threads 4
