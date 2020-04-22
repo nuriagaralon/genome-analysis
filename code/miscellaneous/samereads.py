ffe2e4c91e1c5204d366053747c0d693d5a01f90
nuria <60143046+nuriagaralon@users.noreply.github.com>
2020-04-22 14:40:52 +0200

    Compare reads

---
+++ code/miscellaneous/samereads.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+samereads.py
+-------------------
+This script checks if two fastq files are the same
+"""
+
+import sys
+import gzip
+
+total = 0
+same = 0
+with gzip.open(sys.argv[1], 'rt') as f1, gzip.open(sys.argv[2], 'rt') as f2:
+        for l1,l2 in zip(f1,f2):
+            if not l1.startswith('ERR', 1):
+                total += 1
+                if l1 == l2:
+                    same += 1
+
+perc = (same/total)*100
+
+print(perc)
